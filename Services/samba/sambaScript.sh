600be37f83acd86ddfc45ed66300fa1e58a68b17
osboxes.org <osboxes@osboxes.(none)>
2018-08-31 19:02:41 +0100

    modified samba_mod.conf crom cat to cp -TRv

--- Services/samba/sambaScript.sh
+++ Services/samba/sambaScript.sh
@@ -2,7 +2,10 @@
 
 #This will overwrite the contents inside /etc/samba/smb.conf
 sudo updatedb
-cat $PWDthi/Services/Refrencedirectory/smb_config > /etc/ssh/sshd_config
+chattr -i /etc/samba/smb.conf
+chmod 777 /etc/samba/smb.conf
+sudo cp -TRv samba_mod.conf /etc/samba/smb.conf
 systemctl restart samba.service
 sudo service samba restart
+chmod 770 /etc/samba/smb.conf
 
