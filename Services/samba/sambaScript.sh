b0e447216218e44b07e9af7a086cec682b4e7ebd
oemd001 <kennethchen2001@gmail.com>
2018-08-31 09:45:33 -0700

    Added samba and ssh scripts onto services

--- Services/samba/sambaScript.sh
+++ Services/samba/sambaScript.sh
@@ -1,2 +1,7 @@
 #This script will secure samba
 
+#This will overwrite the contents inside /etc/samba/smb.conf
+cat $PWDthi/Services/Refrencedirectory/smb_config > /etc/ssh/sshd_config
+systemctl restart samba.service
+sudo service samba restart
+
